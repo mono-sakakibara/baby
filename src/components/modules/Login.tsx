912c671ddd7598ec76e519926114b18d664131e3
taro.sakakibara <sakakibara@monosus.co.jp>
2021-12-28 10:48:09 +0900

    :lipstick: style(ui): spotifyログイン画面のスタイル改修

--- src/components/modules/Login.tsx
+++ src/components/modules/Login.tsx
@@ -1,14 +1,25 @@
 import { VFC } from 'react'
 import Link from 'next/link'
+import Box from '@mui/material/Box'
+import Card from '@mui/material/Card'
+import CardActions from '@mui/material/CardActions'
+import CardContent from '@mui/material/CardContent'
+import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
 
 export const Login: VFC = () => {
   return (
-    <div className='App'>
-      <header className='App-header'>
+    <>
+      <CardContent>
+        <Typography sx={{ textAlign: 'center' }} variant='h4' component='h2'>
+          Spotifyアカウント（プレミアム）をお持ちの方。
+        </Typography>
+      </CardContent>
+      <CardActions sx={{ justifyContent: 'center' }}>
         <Link href='/api/auth/login'>
           <a className='btn-spotify'>Login with Spotify</a>
         </Link>
-      </header>
-    </div>
+      </CardActions>
+    </>
   )
 }
