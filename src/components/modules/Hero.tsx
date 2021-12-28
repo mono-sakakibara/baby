7786c62aecf2e9868813d4d349905269b63156d1
taro.sakakibara <sakakibara@monosus.co.jp>
2021-12-28 13:54:53 +0900

    :lipstick: style(ui): heroにリポジトリボタンを追加

--- src/components/modules/Hero.tsx
+++ src/components/modules/Hero.tsx
@@ -1,18 +1,36 @@
-import { Typography } from '@mui/material'
+import { Typography, Button } from '@mui/material'
+import Box from '@mui/material/Box'
+import Image from 'next/image'
 
 export const Hero: React.FC = () => {
   return (
     <>
-      <Typography
-        component='h1'
-        variant='h1'
-        align='center'
-        color='text.primary'
-        gutterBottom
-        sx={{ fontWeight: '900' }}
-      >
-        BABY
-      </Typography>
+      <Box>
+        <Typography
+          component='h1'
+          variant='h1'
+          align='center'
+          color='text.primary'
+          sx={{ fontWeight: '900' }}
+        >
+          BABY
+        </Typography>
+        <Button
+          size='small'
+          href='https://github.com/mono-sakakibara/baby'
+          target='_blank'
+          sx={{ display: 'flex', alignItems: 'center' }}
+        >
+          <Image
+            src='/GitHub-Mark-32px.png'
+            alt=''
+            width='24px'
+            height='24px'
+            loading='lazy'
+          />
+          <Typography sx={{ marginLeft: '10px' }}>repository</Typography>
+        </Button>
+      </Box>
     </>
   )
 }
