31ea2a0925b245169bd9ede5d4bcd1baa2cbb604
taro.sakakibara <sakakibara@monosus.co.jp>
2022-02-22 18:21:46 +0900

    fix: basic認証

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -1,14 +1,12 @@
-// eslint-disable-next-line @next/next/no-server-import-in-page
 import { NextRequest, NextResponse } from 'next/server'
 
-export const middleware = (req: NextRequest) => {
+export function middleware(req: NextRequest) {
   const basicAuth = req.headers.get('authorization')
-  //HeaderにAuthorizationが定義されているかをチェック
+
   if (basicAuth) {
     const auth = basicAuth.split(' ')[1]
     const [user, pwd] = Buffer.from(auth, 'base64').toString().split(':')
 
-    // basic認証のUser/Passが、envファイルにある値と同じかをチェック
     if (
       user === process.env.NEXT_PUBLIC_USER &&
       pwd === process.env.NEXT_PUBLIC_PASS
@@ -17,7 +15,6 @@ export const middleware = (req: NextRequest) => {
     }
   }
 
-  // 同じでなければエラーを返す
   return new Response('Auth required', {
     status: 401,
     headers: {
