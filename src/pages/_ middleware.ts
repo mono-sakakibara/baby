2404c84059d1e3b7bc5a04b4de1441b030515e01
taro.sakakibara <sakakibara@monosus.co.jp>
2022-02-22 17:29:41 +0900

    feat: Basic認証

--- src/pages/_ middleware.ts
+++ src/pages/_ middleware.ts
@@ -3,19 +3,21 @@ import { NextRequest, NextResponse } from 'next/server'
 
 export const middleware = (req: NextRequest) => {
   const basicAuth = req.headers.get('authorization')
-
+  //HeaderにAuthorizationが定義されているかをチェック
   if (basicAuth) {
     const auth = basicAuth.split(' ')[1]
-    const [user, password] = Buffer.from(auth, 'base64').toString().split(':')
+    const [user, pwd] = Buffer.from(auth, 'base64').toString().split(':')
 
+    // basic認証のUser/Passが、envファイルにある値と同じかをチェック
     if (
-      user === process.env.BASIC_AUTH_USER &&
-      password === process.env.BASIC_AUTH_PASSWORD
+      user === process.env.NEXT_PUBLIC_USER &&
+      pwd === process.env.NEXT_PUBLIC_PASS
     ) {
       return NextResponse.next()
     }
   }
 
+  // 同じでなければエラーを返す
   return new Response('Auth required', {
     status: 401,
     headers: {
